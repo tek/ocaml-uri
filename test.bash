cbaecacbcfc5318883c62025c7685d453d6ce58f
Torsten Schmits <torstenschmits@gmail.com>
2021-05-20 17:32:05 +0200

    fix docker host param handling

--- test.bash
+++ test.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -n $1 ]]
+if [[ $1 == '-h' ]]
 then
-  host="-H $1"
-  shift 1
+  host="-H $2"
+  shift 2
 fi
 
-docker ${host} run -v /uri-build:$PWD -w /uri-build ocaml $*
+docker ${host} run -v $PWD:/uri-build -w /uri-build ocaml $*
